9abedefd2d45c3398ceede5a79b8325af72ac53b
rayhankinan <rayhankinan@gmail.com>
2021-11-10 02:32:49 +0700

    Error handling

--- Website/app.py
+++ Website/app.py
@@ -18,7 +18,7 @@ compressionRate = None
 
 @app.route('/', methods=['GET'])
 def front_page():
-    return render_template('frontpage.html', byteImage = None, byteImageCompressed = None, pixelDifference = None, compressionTime = None)
+    return render_template('frontpage.html', byteImage = None, byteImageCompressed = None, pixelDifference = None, compressionTime = None, fileName = None)
 
 @app.route('/upload', methods=['POST'])
 def upload_page():
@@ -29,22 +29,25 @@ def upload_page():
     global photoFileExtension
     global compressionRate
 
-    photo = request.files['uploaded-image']
-    photoFileName, photoFileExtension = os.path.splitext(secure_filename(photo.filename))
+    try:
+        photo = request.files['uploaded-image']
+        photoFileName, photoFileExtension = os.path.splitext(secure_filename(photo.filename))
+
+        inMemoryFile = io.BytesIO()
+        photo.save(inMemoryFile)
+        data = np.frombuffer(inMemoryFile.getvalue(), dtype=np.uint8)
+        imageFile = cv2.imdecode(data, 1)
+    except:
+        pass
+
+    imageFileCompressed = None
 
     try:
         compressionRate = int(request.form['compression-rate']) / 100
     except:
         compressionRate = 1
 
-    inMemoryFile = io.BytesIO()
-    photo.save(inMemoryFile)
-    data = np.frombuffer(inMemoryFile.getvalue(), dtype=np.uint8)
-    imageFile = cv2.imdecode(data, 1)
-
-    imageFileCompressed = None
-
-    return redirect('/view')
+    return redirect('/compress')
 
 @app.route('/view', methods=['GET'])
 def view_page():
@@ -53,31 +56,54 @@ def view_page():
     global imageFileCompressed
     global photoFileName
     global photoFileExtension
+    global compressionRate
     
-    if imageFileCompressed == None:
-        _, frameImage = cv2.imencode(photoFileExtension, imageFile)
-        return render_template('frontpage.html', byteImage = "data:image/" + photoFileExtension[1:] + ";base64," + base64.b64encode(frameImage).decode('utf-8'), byteImageCompressed = None, pixelDifference = None, compressionTime = None)
-    else:
-        _, frameImage = cv2.imencode(photoFileExtension, imageFile)
-        _, frameImageCompressed = cv2.imencode(photoFileExtension, imageFileCompressed)
+    _, frameImage = cv2.imencode(photoFileExtension, imageFile)
+    _, frameImageCompressed = cv2.imencode(photoFileExtension, imageFileCompressed)
 
-        delta = cv2.absdiff(imageFile, imageFileCompressed).astype(np.uint8)
-        percentage = round((np.count_nonzero(delta) * 100) / delta, 2)
+    delta = cv2.absdiff(imageFile, imageFileCompressed).astype(np.uint8)
+    percentage = round((np.count_nonzero(delta) * 100) / delta.size, 2)
 
-        waktuEksekusi = 0 # INI NANTI DIUBAH DENGAN WAKTU EKSEKUSI
+    waktuEksekusi = 0 # INI NANTI DIUBAH DENGAN WAKTU EKSEKUSI
 
-        return render_template('frontpage.html', byteImage = "data:image/" + photoFileExtension[1:] + ";base64," + base64.b64encode(frameImage).decode('utf-8'), byteImageCompressed = "data:image/" + photoFileExtension[1:] + ";base64," + base64.b64encode(frameImageCompressed).decode('utf-8'), pixelDifference = percentage, compressionTime = waktuEksekusi)
+    return render_template('frontpage.html', byteImage = "data:image/" + photoFileExtension[1:] + ";base64," + base64.b64encode(frameImage).decode('utf-8'), byteImageCompressed = "data:image/" + photoFileExtension[1:] + ";base64," + base64.b64encode(frameImageCompressed).decode('utf-8'), pixelDifference = percentage, compressionTime = waktuEksekusi, fileName = photoFileName + photoFileExtension)
 
 @app.route('/save', methods=['POST'])
 def save_image():
+
+    global imageFile
+    global imageFileCompressed
+    global photoFileName
+    global photoFileExtension
+    global compressionRate
+
     return redirect('/')
 
 @app.route('/remove', methods=['POST'])
 def remove_image():
+
+    global imageFile
+    global imageFileCompressed
+    global photoFileName
+    global photoFileExtension
+    global compressionRate
+
+    photoFileName = None
+    photoFileExtension = None
+
     return redirect('/')
 
-@app.route('/compress', methods=['POST'])
+@app.route('/compress', methods=['GET'])
 def compress_image():
+
+    global imageFile
+    global imageFileCompressed
+    global photoFileName
+    global photoFileExtension
+    global compressionRate
+
+    imageFileCompressed = imageFile # INI NANTI DIUBAH
+
     return redirect('/view')
 
 if __name__ == '__main__':
