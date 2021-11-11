3ad32f0581c0a5341761cfffcd04be9927f3a231
Sarah Azka Arief <sarah_azka@yahoo.com>
2021-11-12 02:58:44 +0700

    handle png + add try/except

--- Website/SVD.py
+++ Website/SVD.py
@@ -35,22 +35,23 @@ def svd(matrix, k):
 
     return u, sigma, vt
 
-def compress(img, percentage): #add img as param later
+def compress(percentage): #add img as param later
     # just for testing -- delete this part later
-    #currDir = os.path.dirname(__file__)
-    #path = os.path.join(currDir, './static/images/bjir.jpg') # testing aja nnt apush
-    #tes = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-    #cv2.imshow("yey", tes)
-    #cv2.waitKey()
-    img = img.astype(np.float32)
-    #print(img.shape)
+    currDir = os.path.dirname(__file__)
+    path = os.path.join(currDir, './static/images/jvv absen.jpg') # testing aja nnt apush
+    tes = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+    cv2.imshow("yey", tes)
+    cv2.waitKey()
+    img = tes.astype(np.float32)
+    print(img.shape)
     # delete until here
 
     # initialize rgb
     b = img[:, :, 0]
     g = img[:, :, 1]
     r = img[:, :, 2]
-    #a = img[:, :, 3]
+    try: a = img[:, :, 3]
+    except: pass
     
     # make scale out of percentage
     if (img.shape[0] < img.shape[1]):
@@ -63,7 +64,8 @@ def compress(img, percentage): #add img as param later
     ub, sb, vb = svd(b, k)
     ug, sg, vg = svd(g, k)
     ur, sr, vr = svd(r, k)
-    #ua, sa, va = svd(a, k)
+    try: ua, sa, va = svd(a, k)
+    except: pass
     #print(ub.shape)
     #print(sb.shape)
     #print(vb.shape)
@@ -72,20 +74,22 @@ def compress(img, percentage): #add img as param later
     rScaled = np.dot(ur, np.dot(sr, vr))
     gScaled = np.dot(ug, np.dot(sg, vg))
     bScaled = np.dot(ub, np.dot(sb, vb))
-    #aScaled = np.dot(ua, np.dot(sa, va))
+    try: aScaled = np.dot(ua, np.dot(sa, va))
+    except: pass
 
     # insert compressed r, g, b to matrix
-    imgScaled = cv2.merge((bScaled, gScaled, rScaled))
+    try: imgScaled = cv2.merge((bScaled, gScaled, rScaled, aScaled))
+    except: imgScaled = cv2.merge((bScaled, gScaled, rScaled))
     #imgScaled[:, :, 3] = aScaled
 
     # check for values outside of range of RGB (0-255)
     imgScaled[imgScaled > 255] = 255
     imgScaled[imgScaled < 0] = 0
-    return (imgScaled.astype(np.uint8))
+    #cv2.imwrite("yayoyo.jpg", imgScaled)
+    return (imgScaled)
     #imgScaled = imgScaled.astype(float) / 255
     #cv2.imshow("INI PAKE SVD KITA BOUSZ", imgScaled)
     #cv2.waitKey()
-    #cv2.imwrite("lol.jpg", imgScaled)
 '''
 # uncomment ini kalo mo bandingin jawaban pake svd linalg library
     ur, sr, vr = np.linalg.svd(r)
@@ -117,7 +121,7 @@ def compress(img, percentage): #add img as param later
     #cv2.imshow("INI PAKE SVD LIBRARY BOUSZ", imgScaled)
     #cv2.waitKey()
 '''
-#compress(3)
+compress(3)
 '''
 # ini buat ngetes SVD in general aja
 a = [[1.02650, 0.92840, 0.54947, 0.98317, 0.71226, 0.55847], [0.92889, 0.89021, 0.49605, 0.93776, 0.62066, 0.52473], [0.56184, 0.49148, 0.80378, 0.68346, 1.02731, 0.64579], [0.98074, 0.93973, 0.69170, 1.03432, 0.87043, 0.66371], [0.69890, 0.62694, 1.02294, 0.87822, 1.29713, 0.82905], [0.56636, 0.51884, 0.65096, 0.66109, 0.82531, 0.55098], [1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6]] #ganti jadi matrix apapun itu
