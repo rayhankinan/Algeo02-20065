243b0416663946ba94d284cc3c5962ec0af70b83
dhikaarta <16520019@std.stei.itb.ac.id>
2021-11-12 13:31:46 +0700

    used pil

--- Website/SVD.py
+++ Website/SVD.py
@@ -1,6 +1,7 @@
 import os # ini nanti ilangin cm buat testing
 import cv2
 import numpy as np
+from PIL import Image
 from Eigen import simultaneous_power_iteration
 import math
 
@@ -35,15 +36,15 @@ def svd(matrix, k):
 
     return u, sigma, vt
 
-def compress(percentage): #add img as param later
+def compress(img,percentage): #add img as param later
     # just for testing -- delete this part later
-    currDir = os.path.dirname(__file__)
-    path = os.path.join(currDir, './static/images/jvv absen.jpg') # testing aja nnt apush
-    tes = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-    cv2.imshow("yey", tes)
-    cv2.waitKey()
-    img = tes.astype(np.float32)
-    print(img.shape)
+    #currDir = os.path.dirname(__file__)
+    #path = os.path.join(currDir, './static/images/jvv absen.jpg') # testing aja nnt apush
+    #tes = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+    #cv2.imshow("yey", tes)
+    #cv2.waitKey()
+    img = img.astype(np.float32)
+    #print(img.shape)
     # delete until here
 
     # initialize rgb
@@ -78,8 +79,17 @@ def compress(percentage): #add img as param later
     except: pass
 
     # insert compressed r, g, b to matrix
-    try: imgScaled = cv2.merge((bScaled, gScaled, rScaled, aScaled))
-    except: imgScaled = cv2.merge((bScaled, gScaled, rScaled))
+    try: 
+        redImg = (Image.fromarray(rScaled)).convert("L")
+        greenImg = (Image.fromarray(gScaled)).convert("L")
+        blueImg = (Image.fromarray(bScaled)).convert("L")
+        alphaImg = (Image.fromarray(aScaled)).convert("L")
+        imgScaled = Image.merge("RGB" ,(redImg,greenImg,blueImg, alphaImg))
+    except: 
+        redImg = (Image.fromarray(rScaled)).convert("L")
+        greenImg = (Image.fromarray(gScaled)).convert("L")
+        blueImg = (Image.fromarray(bScaled)).convert("L")
+        imgScaled = Image.merge("RGB" ,(redImg,greenImg,blueImg))
     #imgScaled[:, :, 3] = aScaled
 
     # check for values outside of range of RGB (0-255)
@@ -121,7 +131,7 @@ def compress(percentage): #add img as param later
     #cv2.imshow("INI PAKE SVD LIBRARY BOUSZ", imgScaled)
     #cv2.waitKey()
 '''
-compress(3)
+#compress(3)
 '''
 # ini buat ngetes SVD in general aja
 a = [[1.02650, 0.92840, 0.54947, 0.98317, 0.71226, 0.55847], [0.92889, 0.89021, 0.49605, 0.93776, 0.62066, 0.52473], [0.56184, 0.49148, 0.80378, 0.68346, 1.02731, 0.64579], [0.98074, 0.93973, 0.69170, 1.03432, 0.87043, 0.66371], [0.69890, 0.62694, 1.02294, 0.87822, 1.29713, 0.82905], [0.56636, 0.51884, 0.65096, 0.66109, 0.82531, 0.55098], [1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6], [1,2,3,4,5,6]] #ganti jadi matrix apapun itu
